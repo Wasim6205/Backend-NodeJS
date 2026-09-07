0a101d47b2872fc38963e884c373cbfee9957f51
Wasim6205 <wasimbhai6205@gmail.com>
2026-09-07 22:10:27 +0530

    add cookie and write login controller

--- cohort_3.0_auth/src/app/app.js
+++ cohort_3.0_auth/src/app/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser"
 
 import userRoutes from "../routes/auth.route.js"
 
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/auth',userRoutes)
 
--- cohort_3.0_auth/src/controllers/auth.controller.js
+++ cohort_3.0_auth/src/controllers/auth.controller.js
@@ -28,8 +28,64 @@ export const register = async (req,res) => {
             password:hashedPassword
         })
 
-        const token = jwt.sign({},print.env.JWT_SECRET)
+        const token = jwt.sign({id:newUser._id},process.env.JWT_SECRET)
+
+        res.cookie("token",token)
+
+        return res.status(201).json({
+            success:true,
+            message:"user is created",
+            newUser,
+            token
+        })
     } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({
+            success:false,
+            message:"internal server error"
+        })
+    }
+}
+
+
+export const login = async (req,res) => {
+    try {
+        const {email,password} = req.body
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required"
+            })
+        }
+
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message:"invalid email and password"
+            })
+        }
+
+        const comparePassword = await bcrypt.compare(password,user.password)
+        if(!comparePassword){
+            return res.status(400).json({
+                success:false,
+                message:"invalid email and password"
+            })
+        }
+
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+
+        return res.status(200).json({
+            success:true,
+            message:"user loggedin successfully",
+            user,
+            token
+        })
+
+    } catch (error) {
+        console.log(error.message);
+        
         return res.status(500).json({
             success:false,
             message:"internal server error"
